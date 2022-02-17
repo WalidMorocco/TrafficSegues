9bae448b5d1fac33999c2b1fe0f05dfcd2c961b8
WalidMorocco <walidamarpro@gmail.com>
2022-02-16 20:13:12 -0500

    Traffic Segues Class Assignment

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -13,7 +13,18 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBOutlet weak var segueSwitch: UISwitch!
+    
+    @IBAction func yellowButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn{
+            performSegue(withIdentifier: "Yellow", sender: nil)
+        }
+    }
+    @IBAction func greenButtonTapped(_ sender: Any) {
+        if segueSwitch.isOn{
+            performSegue(withIdentifier: "Green", sender: nil)
+        }
+    }
 }
 
